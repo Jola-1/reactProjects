56491ba6431f98ce1d10d64cd24e9e62e83d4f9d
Jola-1 <adunolamjay6@gmail.com>
2021-03-11 02:23:11 +0100

    modified counter app

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,14 +3,22 @@ import React, { Component } from 'react';
 
 class Counter extends Component{
   state = {
-    count : 1
+    count : 0,
+    tags : ['tag1', 'tag2', 'tag3'],
+    
   };
-  render(){
-    return (<React.Fragment>
-      <span>{this.state.count}</span>
-      <button>Increament</button>
-      </React.Fragment>);
+  
+  renderTags(){
+      if(this.state.tags.length === 0) return <p>There are no tags!</p>;
+      return (<ul>{this.state.tags.map(tag => <li key= {tag}>{tag}</li>) }</ul>);
   }
+  
+  render(){
+    return (<div>
+     {this.renderTags()}
+      </div>);
+  }
+  
 }
 
 export default Counter;
